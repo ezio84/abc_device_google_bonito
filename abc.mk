3901f2527f1ddd02d33694a29c02b4ba4196def3
ezio84 <brabus84@gmail.com>
2021-03-02 21:57:45 +0100

    Update fingerprint to RQ2A.210305.006 7119741

--- abc.mk
+++ abc.mk
@@ -29,12 +29,12 @@ PRODUCT_MODEL := Pixel 3a XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="bonito" \
-    PRIVATE_BUILD_DESC="bonito-user 11 RQ1A.210205.004 7038034 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := "google/bonito/bonito:11/RQ1A.210205.004/7038034:user/release-keys"
+BUILD_FINGERPRINT := "google/bonito/bonito:11/RQ2A.210305.006/7119741:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/bonito/bonito:11/RQ1A.210205.004/7038034:user/release-keys
+    ro.build.fingerprint=google/bonito/bonito:11/RQ2A.210305.006/7119741:user/release-keys
 
 # Active Edge
 PRODUCT_PACKAGES += \
